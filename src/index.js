31b0525842a77cb94d56b54befd10ead11ed8aab
EtsukoSusui <btsm0714@gmail.com>
2023-02-12 15:48:51 +0900

    code split

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import * as AModule from './another-module.js';
+
+
+function component() {
+    const element = document.createElement('div');
+  
+    const btn = document.createElement('button');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
+    btn.innerHTML = 'Click me and check the console!';
+    
+    element.appendChild(btn);
+
+    return element;
+  }
+  
+  document.body.appendChild(component());
